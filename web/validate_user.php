9cc2f4ab2cd5613114bd78ca9e364820a75b7eb8
AlbertDayn <Albert.Dayn@Colorado.edu>
2017-12-12 19:39:42 -0700

    Fixed signin bug.

--- web/validate_user.php
+++ web/validate_user.php
@@ -5,9 +5,9 @@ $conn=mysqli_connect("127.0.0.1","root","","tetrais");
 if ($conn->connect_error) {
     echo "<p>Connection failed:</p>";
 }
-$user_query = "select username, password_hash from users where username = ".$user." and password_hash = ".$pass.";";
+$user_query = "select username, password_hash from users where username = '".$user."' and password_hash = '".$pass."';";
 $user_result_set = @mysqli_query($conn, $user_query);
-$users = mysqli_fetch_array($user_result_set,MYSQLI_NUM);
+$users = mysqli_fetch_array($user_result_set);
 if (count($users) == 0) {
     include 'signin.php';
 }
